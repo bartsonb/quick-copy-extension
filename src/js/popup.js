503ca08da8664a7917bb23b02aa7fd5c0975cc7b
Armin Bartnik <armin.bartnik@web.de>
2020-04-24 14:12:45 +0200

    added support for multiple languages

--- src/js/popup.js
+++ src/js/popup.js
@@ -9,11 +9,21 @@ const characters = {
         'aring',
         'aelig',
         'oslash'
+    ],
+    "de": [
+        'szlig',
+        'auml',
+        'ouml',
+        'uuml',
+        'Auml',
+        'Ouml',
+        'Uuml'
     ]
 }
 
 const countryCodes = {
-    "no": "Norwegian"
+    "no": ['Norwegian', 'Norsk'],
+    "de": ['German', 'Deutsch']
 }
 
 const DOM = {
@@ -25,18 +35,22 @@ const DOM = {
 }
 
 const templates = {
-    wrapper: '<div class=\'char-group\'><p class=\'char-group__title\'>~language~</p></div>',
-    character: '<button class=\'char-group__character ~code~\'>&~code~;</button>'
+    characterGroup: "<div class='char-group ~language~'><p class='char-group__title'>~language~</p><div class='char-group__content'></div></div>",
+    character: "<button class='char-group__content__button ~code~'>&~code~;</button>"
 }
 
-let notify = ( msg, duration = null ) => {
+let notify = ( msg, close, duration = null ) => {
     DOM.message.innerHTML = msg;
     DOM.overlay.style.display = "flex";
 
     setTimeout(() => {
         DOM.overlay.style.opacity = 1;
 
-        if (duration) setTimeout(() => { window.close() }, duration);
+        if (duration) setTimeout(() => { 
+            if (close) window.close();
+
+            DOM.overlay.style.opacity = 0;
+        }, duration);
     }, 30);
 }
 
@@ -48,7 +62,7 @@ let clickHandler = ({ target }) => {
 
     navigator.clipboard.writeText(character)
         .then(() => {
-            notify(character + " " + chrome.i18n.getMessage("copiedSuccessfully"), 1000);
+            notify(character + " " + chrome.i18n.getMessage("copiedSuccessfully"), false, 1000);
         })
         .catch(err => {
             console.log(err)
@@ -65,21 +79,24 @@ DOM.optionsPageLink.addEventListener("click", () => {
 });
 
 // Build Page
-chrome.storage.sync.set({ selectedLanguages: null });
+chrome.storage.sync.set({ selectedLanguages: ["de", "no"] });
 
 chrome.storage.sync.get(['selectedLanguages'], ({ selectedLanguages }) => {
     if (selectedLanguages) {
         selectedLanguages.forEach(lang => {
-            let wrapper = createFromTemplate(templates.wrapper);
-    
-            characters[lang].forEach(char => {
-                wrapper.insertAdjacentElement(
-                    "afterbegin", 
-                    createFromTemplate(templates.character.replace(/~code~/g, char))
+            let [ language ] = countryCodes[lang];
+
+            DOM.target.insertAdjacentElement(
+                "beforeEnd", 
+                createFromTemplate(templates.characterGroup.replace(/~language~/g, language))
+            );
+
+            characters[lang].map(char => {
+                document.querySelector(`.char-group.${language} > .char-group__content`).insertAdjacentElement(
+                    "afterBegin", 
+                    createFromTemplate(templates.character.replace(/~code~/g, char), clickHandler)
                 );
             });
-    
-            DOM.target.insertAdjacentElement("afterend", wrapper);
         });
     } else {
         DOM.description.innerText = "Please select a language in the configuration page.";
